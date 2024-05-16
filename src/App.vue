e721785894e27f5dd518caac86113c1f58c90373
Sarwan Nizamani <sarwan.nizamani8@gmail.com>
2024-05-16 15:47:07 -0700

    finilize card design

---
+++ src/App.vue
@@ -0,0 +1,148 @@
+<script setup>
+import IconBrokenChain from "@/components/icons/IconBrokenChain.vue";
+import IconInbox from "@/components/icons/IconInbox.vue";
+</script>
+
+<template>
+  <div class="card-container">
+    <div class="card" v-for="card in 4" :key="card">
+      <div class="inbox__icon">
+        <div class="icon-container">
+          <div class="icon">
+            <IconInbox />
+          </div>
+        </div>
+
+
+      </div>
+      <div class="coming-soon">
+        <IconBrokenChain />
+        <p class="coming-soon__heading">Coming soon!</p>
+        <p class="coming-soon__description">Stay tuned for future updates!</p>
+      </div>
+      <div class="card-content">
+
+        <div class="card-headings">
+          <h3 class="card-headings__heading">Inbox</h3>
+          <span class="availability available">Available</span>
+        </div>
+        <p class="card-description">
+          Manage support tickets, requests, and customer communication, all from one
+          inbox.
+        </p>
+        <p class="category">Customer support software</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card-container {
+  display: flex;
+  gap: 20px;
+}
+
+.card {
+  background-color: #fff;
+  border-radius: 20px;
+  width: 360px;
+  overflow: hidden;
+  padding: 10px;
+  position: relative;
+}
+
+.inbox__icon {
+  position: absolute;
+  top: 125px;
+  left: 20px;
+}
+
+
+.icon-container {
+  width: 48px;
+  height: 48px;
+  border-radius: 12px;
+  background-color: #eff0f3a6;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.icon {
+  width: 38px;
+  height: 38px;
+  background-color: white;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 999px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card:hover {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  transition: all cubic-bezier(0.23, 1, 0.32, 1);
+  padding: 10px;
+}
+
+.coming-soon {
+  text-align: center;
+  padding: 20px;
+  background-color: #f9f9fb;
+  border-radius: 16px;
+}
+
+.coming-soon__heading {
+  font-size: medium;
+  font-weight: 900;
+  color: #1e1f24;
+  margin-block-start: 20px;
+  margin-block-end: 10px;
+}
+
+.coming-soon__description {
+  color: #62636c;
+  font-size: 14px;
+  margin-block-start: 10px;
+  margin-block-end: 10px;
+}
+
+.card-headings {
+  display: flex;
+  justify-content: space-between;
+}
+
+.card-headings__heading {
+  color: #1e1f24;
+}
+
+.card-content {
+  padding: 10px;
+}
+
+.availability {
+  display: inline-block;
+  padding: 4px 8px;
+  border-radius: 4px;
+  font-size: 12px;
+}
+
+.availability.available {
+  background-color: rgba(2, 179, 2, 0.079);
+  color: #006316;
+  margin: auto 0;
+  border-radius: 999px;
+}
+
+.card-description {
+  color: #62636c;
+  font-size: 14px;
+  width: 244px;
+  text-align: left;
+}
+
+.category {
+  font-size: 12px;
+  color: #1e1f24;
+}
+</style>
---
+++ src/components/icons/IconInbox.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M18 3L6.00004 3C4.34319 3.00001 3.00004 4.34315 3.00004 6L3 18C2.99999 19.6569 4.34314 21 6 21H18C19.6569 21 21 19.6569 21 18V6C21 4.34315 19.6569 3 18 3ZM6.00004 5L18 5C18.5523 5 19 5.44772 19 6V12H15.874C15.4177 12 15.0193 12.3089 14.9055 12.7507C14.5724 14.0449 13.3965 15 12 15C10.6035 15 9.42754 14.0449 9.09445 12.7507C8.98072 12.3089 8.5823 12 8.12601 12H5.00002L5.00004 6C5.00004 5.44772 5.44775 5 6.00004 5Z"
+      fill="url(#paint0_linear_1_1208)"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear_1_1208"
+        x1="12"
+        y1="3"
+        x2="12"
+        y2="21"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#1F2327" />
+        <stop offset="1" stop-color="#60646C" />
+      </linearGradient>
+    </defs>
+  </svg>
+</template>
